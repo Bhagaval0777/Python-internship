509b2070e66e7e23e3395ae22247fa721b2b6277
Bhagaval0777 <bhagaval.krishna@vaisesika.in>
2025-05-26 16:26:31 +0530

    Add files via upload

--- max_count.py
+++ max_count.py
@@ -1,11 +1,8 @@
+from collections import Counter
 nums = [1,2,2,3,1,4,1,2,4,3,1,5,2]
-h ={}
-for i in nums:
-    if i in h:
-        h[i]+=1 
-    else:
-        h[i]=1
-        
-print(h)
-a = max(h,key=h.get)
-print(a)
+h = Counter(nums)
+max_val = max(h.values())
+
+ans = [i for i in h if h[i] == max_val]
+print(ans)
+
