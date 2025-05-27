6e7d0fbf8b7710bce8b1e5330ac5be2b11e484a0
Bhagaval0777 <bhagaval.krishna@vaisesika.in>
2025-05-27 22:12:57 +0530

    Add files via upload

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -4,7 +4,7 @@ def fun(n):
         if n[i]=="(" or n[i]=="{" or n[i]=="[":
             h.append(n[i])
         else:
-            if h and((h[-1]=="(" and n[i]==')')or
+            if ((h[-1]=="(" and n[i]==')')or
               (h[-1]=="{" and n[i]=='}') or
               (h[-1]=="[" and n[i]=="]")):
                 h.pop() 
@@ -14,7 +14,7 @@ def fun(n):
     
 
 
-s = "{([])}"
+s = "{}[]"
 print(fun(s))
 
 
